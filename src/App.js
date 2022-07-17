3ceb4cf5ba04f206c14df77c87d56708ea4158aa
Seojisoo20191941 <amy0480@naver.com>
2022-07-18 02:03:17 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from "./logo.svg";
+// import React, { useState } from "react";
+import "./App.css";
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <main>
+      <Hello />
+      <Hello />
+    </main>
+  );
+
+  // return (
+  //   <main>
+  //     <input
+  //       type="text"
+  //       onChange={(event) => console.log(event.target.value)}
+  //     />
+  //     <button>추가</button>
+  //   </main>
+  // );
+}
+
+export default App;
